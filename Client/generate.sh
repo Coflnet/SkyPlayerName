d18cccf0acb85c27024972ed8f53128140f983d3
Äkwav <ekwav@coflnet.com>
2024-04-24 12:56:20 +0200

    update to .net 8

--- Client/generate.sh
+++ Client/generate.sh
@@ -1,14 +1,19 @@
-VERSION=0.2.1
+VERSION=0.2.2
 
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5030/swagger/v1/swagger.json \
 -g csharp \
--o /local/out --additional-properties=packageName=Coflnet.Sky.PlayerName.Client,packageVersion=$VERSION,licenseId=MIT
+-o /local/out --additional-properties=packageName=Coflnet.Sky.PlayerName.Client,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
 
 cd out
-sed -i 's/GIT_USER_ID/Coflnet/g' src/Coflnet.Sky.PlayerName.Client/Coflnet.Sky.PlayerName.Client.csproj
-sed -i 's/GIT_REPO_ID/SkyPlayerName/g' src/Coflnet.Sky.PlayerName.Client/Coflnet.Sky.PlayerName.Client.csproj
-sed -i 's/>OpenAPI/>Coflnet/g' src/Coflnet.Sky.PlayerName.Client/Coflnet.Sky.PlayerName.Client.csproj
+path=src/Coflnet.Sky.PlayerName.Client/Coflnet.Sky.PlayerName.Client.csproj
+sed -i 's/GIT_USER_ID/Coflnet/g' $path
+sed -i 's/GIT_REPO_ID/SkyPlayerName/g' $path
+sed -i 's/>OpenAPI/>Coflnet/g' $path
+
+sed -i 's@annotations</Nullable>@annotations</Nullable>\n    <PackageReadmeFile>README.md</PackageReadmeFile>@g' $path
+sed -i 's@Remove="System.Web" />@Remove="System.Web" />\n    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>@g' $path
+
 
 dotnet pack
-cp src/Coflnet.Sky.PlayerName.Client/bin/Debug/Coflnet.Sky.PlayerName.Client.*.nupkg ..
+cp src/Coflnet.Sky.PlayerName.Client/bin/Release/Coflnet.Sky.PlayerName.Client.*.nupkg ..
--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM mcr.microsoft.com/dotnet/sdk:7.0 as build
+FROM mcr.microsoft.com/dotnet/sdk:8.0 as build
 WORKDIR /build
 RUN git clone --depth=1 https://github.com/Coflnet/HypixelSkyblock.git dev
 WORKDIR /build/sky
@@ -7,14 +7,13 @@ RUN dotnet restore
 COPY . .
 RUN dotnet publish -c release -o /app
 
-FROM mcr.microsoft.com/dotnet/aspnet:7.0
+FROM mcr.microsoft.com/dotnet/aspnet:8.0-alpine
 WORKDIR /app
 
 COPY --from=build /app .
 
 ENV ASPNETCORE_URLS=http://+:8000
 
-RUN useradd --uid $(shuf -i 2000-65000 -n 1) app
 USER app
 
 ENTRYPOINT ["dotnet", "SkyPlayerName.dll", "--hostBuilder:reloadConfigOnChange=false"]
